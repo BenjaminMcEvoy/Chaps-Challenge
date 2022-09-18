054e0c6da0d6b2c187392badb518c3cd8aa532bc
Adam <Adam@DESKTOP-DFU19S6.hub>
2022-09-18 17:52:27 +1200

    tile, location and Walltile class started

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Tile.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Tile.java
@@ -1,39 +1,50 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
 
-/** 
+/**
  * The Domain interface for all tiles within the game
- * 
+ *
  * */
 public abstract class Tile{
-	
+
 	// Fields
-	private int x;
-	private int y;
-	private int lastX;
-	private int lastY;
-	
+	private Location location;
+	protected boolean walkable = true;
+
+
 	/**
 	 * Construct a tile at given coordinates
-	 * 
+	 *
 	 * @param x - x coordinate
 	 * @param y - y coordinate
-	 * 
+	 *
 	 * */
 	Tile(int x, int y){
-		this.x = x;
-		this.y = y;
+		this.location = new Location(this,x,y);
+	}
+
+	public void moveUp() {
+		location.move(location.getRow(), location.getColumn()-1);
+	}
+
+	public void moveDown() {
+		location.move(location.getRow(), location.getColumn()+1);
+	}
+
+	public void moveLeft() {
+		location.move(location.getRow()-1, location.getColumn());
+	}
+
+	public void moveRight() {
+		location.move(location.getRow()+1, location.getColumn());
 	}
 	
-	public int getX() {
-		return x;
-	}
+	//will call this method to draw the tile
 	
-	public int getY() {
-		return y;
-	}
-	
-	
-	
-	
+	public abstract void draw();
+
+
+
+
+
 }
