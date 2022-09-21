8ff04291fd1b21ac53bed5b5b6c8594cee81c12b
Ethan <windleetha@myvuw.ac.nz>
2022-09-22 11:21:44 +1200

    Added recorder

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -0,0 +1,20 @@
+package nz.ac.vuw.ecs.swen225.gp22.recorder;
+
+public class Recorder {
+
+	public void SaveGame() {
+		
+	}
+	public void LoadGame() {
+		
+	}
+	public void Next() {
+		
+	}
+	public void Previous() {
+		
+	}
+	public void AutoPlay() {
+		
+	}
+}
