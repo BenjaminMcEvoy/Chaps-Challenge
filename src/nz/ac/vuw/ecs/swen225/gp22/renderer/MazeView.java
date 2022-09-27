f986f31392874cec34041b10d4fda345e8761fde
Benjamin McEvoy <mcevoybenj@ecs.vuw.ac.nz>
2022-09-27 17:37:21 +1300

    minor updates and merges
    Created an animate file for any animation in the package
    
    Merged all sound java files into one, for easier imports and did some code
    
    MazeView file just added some comments and code

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/MazeView.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/MazeView.java
@@ -1,6 +1,15 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
 import javax.swing.JPanel;
+import javax.swing.*;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
 
 /** 
  * Maze View class to update and render the maze whenever
@@ -8,10 +17,18 @@ import javax.swing.JPanel;
  * 
  * 
  * */
-@SuppressWarnings("serial")
-public class MazeView extends JPanel{
+public class MazeView extends JPanel implements ActionListener{
 	
-	MazeView(){
+	// Fields
+	private int bWidth, bHeight, width, height, tSize, tView;
+	private Maze maze;
+	private Tile[][] board;
+	
+	MazeView(Maze m){
+		init();
+		maze = m;
+		//width = m;
+		//height = m.;
 		
 		/* Initialization of fields possibly
 		 * set the parameters of the view
@@ -20,6 +37,13 @@ public class MazeView extends JPanel{
 		 * */
 	}
 	
+	private void init() {
+		bWidth = 850;
+		bHeight = 850;
+		tSize = 50;
+		tView = 5;
+	}
+
 	/**
 	 * Draws the main character with coordinates recorded by
 	 * the character itself
@@ -42,17 +66,56 @@ public class MazeView extends JPanel{
 	 * 
 	 * 
 	 * */
-	public void renderTiles() {return;}
+	public void renderTiles(Graphics graph) {
+		for(int x = 0; x < height; x++) {
+			for(int y = 0; y< width; y++) {
+				Tile tile = maze.getTileAt(x, y);
+				//graph.drawImage();// fill here
+				
+				// maybe check instance of tile or use use a tile precheck
+				// to see if it contains any entities or objects at the start
+				
+				// if there is check all entities/containables
+			}
+			
+		}
+		
+		}
 	
 	
 	
 	
 	/**
 	 *  Get the maze/board/pane
+	 * @return maze
 	 * 
 	 * */
-	public void getMaze() {
-		return;
+	public Maze getMaze() {
+		return maze;
+	}
+
+	/**
+	 *  Get the maze/board/pane width
+	 * @return bWidth
+	 * 
+	 * */
+	public int getbWidth() {
+		return bWidth;
+	}
+
+	/**
+	 *  Get the maze/board/pane height
+	 * @return bHeight
+	 * 
+	 * */
+	public int getbHeight() {
+		return bHeight;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 		
 }
