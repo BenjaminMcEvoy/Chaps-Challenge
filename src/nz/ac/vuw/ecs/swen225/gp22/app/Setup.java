9273cb1aafb621abebfbb32ae2519d850e6dbfc1
Giancarlo Cigaral <cigaragian1@ecs.vuw.ac.nz>
2022-09-29 16:54:34 +1300

    Update src/nz/ac/vuw/ecs/swen225/gp22/app/Setup.java, src/nz/ac/vuw/ecs/swen225/gp22/app/Game.java, src/nz/ac/vuw/ecs/swen225/gp22/app/Instructions.java

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Setup.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Setup.java
@@ -16,11 +16,14 @@ import java.io.IOException;
  */
 public class Setup extends JFrame implements ActionListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JButton newGame;
 	JButton loadGame;
 	JButton instructions;
 	JButton quit;
-	JButton back;
 	JPanel panel;
 	
 	Runnable clearPanel = ()->{
@@ -79,27 +82,29 @@ public class Setup extends JFrame implements ActionListener{
 			// TO DO :
 			//		- Create new game from level selected using xml files
 			// 		- Would create a new instance of some persistency object like a "GameLoader" object
+			//		- Ideally "GameLoader" object should parse a given file, create instances of each
+			//		  tile and 
+			this.dispose();
+			new Game();
+			
 		}
+		
 		//user clicks on loadGame
 		else if (e.getSource().equals(loadGame)) {
 			System.out.println("Load Game");
 			// TO DO :
 			//		- Load either a new game at level 1/2 or load a saved game using xml files
-			//		- Would create a new instance of some persistency object like a "GameLoader" object
 		}
+		
 		//user clicks on instructions
 		else if (e.getSource().equals(instructions)) {
 			System.out.println("Instructions");
 			clearPanel.run();
-			instructions();
-		}
-		//user clicks on back
-		else if(e.getSource().equals(back)) {
-			System.out.println("Back");
-			remove(back);
-			clearPanel.run();
-			mainMenu();
-		}
+			this.dispose();
+			new Instructions();
+			
+		}		
+		
 		//user clicks on quit
 		else if(e.getSource().equals(quit)) {
 			System.out.println("Quit");
@@ -116,42 +121,5 @@ public class Setup extends JFrame implements ActionListener{
 				mainMenu();
 			}
 		}
-	}
-	
-	public void instructions() {
-		this.setMinimumSize(new Dimension(410, 210));
-		var up = new JLabel("Up = Up arrow key");
-		var down = new JLabel("Down = Down arrow key");
-		var left = new JLabel("Left = Left arrow key");
-		var right = new JLabel("Right = Right arrow key");
-		var pause = new JLabel("Pause = Space key");
-		var unpause = new JLabel("Unpause = Escape key");
-		var exit = new JLabel("Exit without saving = CTRL + X");
-		var exsv = new JLabel("Exit and save = CTRL + S");
-		var reload = new JLabel("Reload saved game = CTRL + R");
-		var gameOne = new JLabel("Start new game at Lvl 1 = CTRL + 1");
-		var gameTwo = new JLabel("Start new game at Lvl 2 = CTRL + 2");
-		panel = new JPanel(new GridLayout(6, 2));
-		back = new JButton("Back");
-		back.addActionListener(this);
-
-		panel.add(up);
-		panel.add(down);
-		panel.add(left);
-		panel.add(right);
-		panel.add(pause);
-		panel.add(unpause);
-		panel.add(exit);
-		panel.add(exsv);
-		panel.add(reload);
-		panel.add(gameOne);
-		panel.add(gameTwo);
-		add(back);
-		
-		add(BorderLayout.CENTER, panel);
-		add(BorderLayout.SOUTH, back);
-		add(panel);
-		pack();
-	}
-	
+	}	
 }
