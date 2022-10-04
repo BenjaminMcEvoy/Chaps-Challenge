09a5f92033372209e91bc65b7cf34e57c942aff2
Benjamin McEvoy <mcevoybenj@ecs.vuw.ac.nz>
2022-10-04 15:06:45 +1300

    Fixed file, Visual Studio code bugged out

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/Sound.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/Sound.java
@@ -1,8 +1,7 @@
-package nz.ac.vuw.ecs.swen225.gp22.renderer;
+spackage nz.ac.vuw.ecs.swen225.gp22.renderer;
 
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 import java.io.*;
-<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -71,65 +70,5 @@ public class Sound {
 		clip.start();
 	}
 
-=======
-import javax.sound.sampled.*;
-
-public interface Sound<T>{
-	
-		public void load(T t);
-		
-		public void play(T t);
-		
-}
-
-class MobSound implements Sound<Tile>{
-	
-	Clip sound;
-
-
-	@Override
-	public void load(Tile t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void play(Tile t) {
-		// TODO Auto-generated method stub
-		if(t instanceof ChapTile) {
-			if(t.isWalkable()) {
-				// play walk sound
-			} else{
-				// play i cant go there maybe? or remove and not worry about it
-				// depends if find a royalty free source
-			}
-		}
-		
-	}
-	
-}
-
-class AmbientSound implements Sound<Tile>{
-	
-
-	@Override
-	public void load(Tile t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void play(Tile t) {
-		// TODO Auto-generated method stub
-		if(t instanceof KeyTile) {
-			// play key sound
-		} else if(t instanceof LockedDoorTile) {
-			if(!t.isWalkable()) {
-				// play locked sound
-			}
-		}
-		
-	}
->>>>>>> 4724410b63643ca43f8c7f1b23c42a778475b840
 	
 }
