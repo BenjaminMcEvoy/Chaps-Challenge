35bd9c1a39131c1170c22dded9f982fc96098e2f
Benjamin McEvoy <mcevoybenj@ecs.vuw.ac.nz>
2022-10-04 14:48:13 +1300

    Revert "Merge branch 'main' of..."
    This reverts commit 0a4546590045867d8d09ead0701f436e8e7002b1

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Tile.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Tile.java
@@ -1,78 +1,39 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
 
-/**
+/** 
  * The Domain interface for all tiles within the game
- *
+ * 
  * */
 public abstract class Tile{
-
+	
 	// Fields
-	protected Maze maze;
-	//private Location location;
-	//private int x, y;
-	protected boolean walkable = true;
-	protected boolean moveable = false;
-
-
+	private int x;
+	private int y;
+	private int lastX;
+	private int lastY;
+	
 	/**
 	 * Construct a tile at given coordinates
-	 *
+	 * 
 	 * @param x - x coordinate
 	 * @param y - y coordinate
-	 *
+	 * 
 	 * */
 	Tile(int x, int y){
-		maze.setTile(this, x, y);
-	}
-
-	public void moveUp() {
-		maze.moveTile(this, getX(),getY()-1);
-	}
-
-	public void moveDown() {
-		maze.moveTile(this, getX(),getY()+1);
-	}
-
-	public void moveLeft() {
-		maze.moveTile(this, getX()-1,getY());
-	}
-
-	public void moveRight() {
-		maze.moveTile(this, getX()+1,getY());
-	}
-	
-	
-	//getters
-	
-	public Maze getMaze() {
-		return maze;
+		this.x = x;
+		this.y = y;
 	}
 	
 	public int getX() {
-		return maze.getTileX(this);
+		return x;
 	}
 	
 	public int getY() {
-		return maze.getTileY(this);
+		return y;
 	}
 	
-	public boolean isWalkable() {
-		return walkable;
-	}
-	
-	public boolean isMoveable() {
-		return moveable;
-	}
-	
-	//will call this method to draw the tile
-	
-	public abstract void draw();
-
-	public abstract String toString();
-		
 	
 	
-
-
+	
 }
--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -1,72 +1,20 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
-import nz.ac.vuw.ecs.swen225.gp22.renderer.Animate.direction;
-import nz.ac.vuw.ecs.swen225.gp22.domain.*;
-
-import java.io.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.*;
-
-import org.w3c.dom.*;
-import java.util.Stack;
 
 public class Recorder {
-	static boolean auto = false;
-	static int playbackSpeed = 10;
-	static File file;
-	
-	public void SaveGame(Maze m, String newFileName) {
-		if(file == null) {System.out.println("Nothing to be saved");}
-		try {
-			InputStream oldFile = new FileInputStream(file);
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document newFile = db.parse(oldFile);
-			
-			NodeList characters = newFile.getElementsByTagName("character");
-			for(int i=0; i<characters.getLength(); i++) {
-				Node current = characters.item(i);
-				if (current.getNodeType() == Node.ELEMENT_NODE) {
-					String name = current.getAttributes().getNamedItem("name").getTextContent();
-					CharacterTile chara = null;
-					for(CharacterTile c: m.getCharacters()) {
-						if(c.getName().equals(name)) {chara = c;}
-					}
-					String text = "";
-					Stack<direction> moves = (Stack<direction>) chara.getPrevMoves().clone();
-					if(!moves.isEmpty()) {text += moves.pop().toString();}
-					while(!moves.isEmpty()) {text += ", " + moves.pop().toString();}
-					Node move = current.getLastChild();
-					move.setTextContent(text);
-				}
-			}
-			FileOutputStream outputFile = new FileOutputStream(newFileName+"xml");
-			TransformerFactory tf = TransformerFactory.newInstance();
-			Transformer t = tf.newTransformer(new StreamSource(new File(newFileName+".xslt")));
-			t.setOutputProperty(OutputKeys.INDENT, "yes");
-	        t.setOutputProperty(OutputKeys.STANDALONE, "no");
-	        DOMSource source = new DOMSource(newFile);
-	        StreamResult result = new StreamResult(outputFile);
-	        t.transform(source, result);
-	        
-		}catch(Exception e) {e.printStackTrace();}
+
+	public void SaveGame() {
+		
 	}
-	
-	
-	public void Next(Maze m) {
-		auto = false;
-		m.getCharacters().stream().forEach(i->i.nextMove());
+	public void LoadGame() {
+		
 	}
-	public void Back(Maze m) {
-		auto = false;
-		m.getCharacters().stream().forEach(i->i.previousMove());
+	public void Next() {
+		
 	}
-	public void AutoPlay(Maze m) throws InterruptedException {
-		auto = true;
-		while(!m.getCharacters().get(0).getNextMoves().isEmpty() && auto) {
-			m.getCharacters().stream().forEach(i->i.nextMove());
-			wait(2000/playbackSpeed);
-		}
+	public void Previous() {
+		
+	}
+	public void AutoPlay() {
+		
 	}
 }
