87d2eb6aa845166e8a1ced6f3610b6f7101dcfae
Ethan <windleetha@myvuw.ac.nz>
2022-10-12 19:00:29 +1300

    loader

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -9,7 +9,13 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.*;
 
+import org.jdom2.*;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import org.w3c.dom.*;
+
+import java.util.List;
 import java.util.Stack;
 
 public class Recorder {
@@ -58,23 +64,45 @@ public class Recorder {
 	public Maze LoadSave(File file) throws Exception {
 		try {
 			// load XML file document
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(file);
+			SAXBuilder sax = new SAXBuilder();
+			Document doc = sax.build(file);
 			
 			//create level
-			Element root = doc.getDocumentElement();
-			String level = root.getAttribute("level");
+			Element root = doc.getRootElement();
+			String level = root.getAttributeValue("level");
 			XMLLoader levelLoader = new XMLLoader();
 			levelLoader.loadFile(new File("src/nz/ac/vuw/ecs/swen225/gp22/recorder/Levels/" + level));
 			
-			//update characters possible movement
+			//update characters future movement
 			Maze maze = levelLoader.getMaze();
-			NodeList nodes = root.getChildNodes();
-			root.
-			for(int i=0; i<nodes.getLength(); i++) {
-				String name = nodes.item(i).
-				maze.getAllEntities().stream().forEach(n->{if(n.getFileName().equals(name)){addMove(n, moves);}};
+			List<Element> elements = root.getChildren();
+			
+			//go through each element
+			for(Element e: elements) {
+				//find the right character
+				String name = e.getAttributeValue("name");
+				for(CharacterTile c: maze.getCharacters()) {
+					if(c.getName().equals(name)){
+						String[] moves = e.getChild("moves").getText().split(", ");
+						//push moves on to characters next moves stack
+						for(String str: moves) {
+							switch(str) {
+								case "up":
+									c.getNextMoves().push(direction.UP);
+									break;
+								case "left":
+									c.getNextMoves().push(direction.LEFT);
+									break;
+								case "down":
+									c.getNextMoves().push(direction.DOWN);
+									break;
+								case "right":
+									c.getNextMoves().push(direction.RIGHT);
+									break;
+							}
+						}
+					}
+				}
 			}
 			return maze;
 		}catch(Exception e){throw e;}
@@ -83,10 +111,6 @@ public class Recorder {
 	public void Next(Maze m) {
 		auto = false;
 		m.getCharacters().stream().forEach(i->i.nextMove());
-	}
-	public void Back(Maze m) {
-		auto = false;
-		m.getCharacters().stream().forEach(i->i.previousMove());
 	}
 	public void AutoPlay(Maze m) throws InterruptedException {
 		auto = true;
