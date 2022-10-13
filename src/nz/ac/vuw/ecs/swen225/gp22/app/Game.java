8e98d4216d9689351ac975ad98158dea6f54005f
Giancarlo Cigaral <cigaragian1@ecs.vuw.ac.nz>
2022-10-13 21:22:17 +1300

    added recorder jmenu items

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Game.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Game.java
@@ -1,66 +1,66 @@
-	package nz.ac.vuw.ecs.swen225.gp22.app;
-	
-	import nz.ac.vuw.ecs.swen225.gp22.domain.*;
-	import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
-	import nz.ac.vuw.ecs.swen225.gp22.persistency.*;
-	import nz.ac.vuw.ecs.swen225.gp22.renderer.*;
-	import nz.ac.vuw.ecs.swen225.gp22.recorder.*;
-	
-	import javax.swing.*;
-	import java.awt.*;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
-	import java.awt.event.WindowAdapter;
-	import java.awt.event.WindowEvent;
-	import java.io.File;
-	
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
+import nz.ac.vuw.ecs.swen225.gp22.persistency.*;
+import nz.ac.vuw.ecs.swen225.gp22.renderer.*;
+import nz.ac.vuw.ecs.swen225.gp22.recorder.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Main class for running game
+ * @author CarloC
+ *
+ */
+public class Game extends JFrame implements ActionListener{
+
 	/**
-	 * Main class for running game
-	 * @author CarloC
-	 *
+	 * 
 	 */
-	public class Game extends JFrame implements ActionListener{
-	
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		private Runnable stop = ()->{};
-		private Runnable restart = ()->{};
-		private Runnable nextLvl = ()->{};
-		private MazeView mv;
-		private JPanel container;
-		private Controller controller;
-		private Maze maze;
-		private File level;
-		private InventoryView iv;
-		private Sound sound = new Sound();
-	
-		private Timer timer;
-		private long startTime;
-		private long duration;
-		
-		public Runnable save = ()->{
-			JFileChooser chooser = new JFileChooser(new File("src/nz/ac/vuw/ecs/swen225/gp22/recorder/SavedGame"));
-			int j = chooser.showSaveDialog(null);
-			if(j == JFileChooser.APPROVE_OPTION) {
-				try {
-					File f = chooser.getSelectedFile();
-					Recorder.SaveGame(maze, f);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			}};
-	
-	
+	private static final long serialVersionUID = 1L;
+	private Runnable stop = ()->{};
+	private Runnable restart = ()->{};
+	private Runnable nextLvl = ()->{};
+	private MazeView mv;
+	private JPanel container;
+	private Controller controller;
+	private Maze maze;
+	private File level;
+	private InventoryView iv;
+
+	private Timer timer;
+	private long startTime;
+	private long duration;
+
+	public Runnable save = ()->{
+		JFileChooser chooser = new JFileChooser(new File("src/nz/ac/vuw/ecs/swen225/gp22/recorder/SavedGame"));
+		int j = chooser.showSaveDialog(null);
+		if(j == JFileChooser.APPROVE_OPTION) {
+			try {
+				File f = chooser.getSelectedFile();
+				Recorder.SaveGame(maze, f);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}};
+
+
 		public Game(Maze m) {
-			
+
 			this.addWindowListener(new WindowAdapter() {
 				public void windowClosing(WindowEvent e) {
 					System.exit(0);
 				}
 			});
-	
+
 			assert SwingUtilities.isEventDispatchThread();
 			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			setLocationRelativeTo(null);
@@ -70,80 +70,80 @@
 			mv.addKeyListener(controller);
 			iv = new InventoryView(maze);
 			level = new File("src/nz/ac/vuw/ecs/swen225/gp22/recorder/Levels/" + maze.getLevel() + ".xml");
-	
+
 			duration = 100000;
 			startTime = -1;
-			
+
 			gui();
 			setVisible(true);
 		}
-		
+
 		/**
 		 * Constructor for loading a particular level - Not working atm?
 		 * @param file specified level to be loaded
 		 */
-		
+
 		public Game(File file) {
-	
+
 			this.addWindowListener(new WindowAdapter() {
 				public void windowClosing(WindowEvent e) {
 					System.exit(0);
 				}
 			});
-	
+
 			assert SwingUtilities.isEventDispatchThread();
 			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			setLocationRelativeTo(null);
 			XMLLoader loader = new XMLLoader();
 			loader.loadFile(file);
-			
+
 			maze = loader.getMaze();
 			mv = new MazeView(maze);
-	
+
 			controller = new Controller(maze);
 			mv.addKeyListener(controller);
-			
+
 			iv = new InventoryView(maze);
 			level = new File("src/nz/ac/vuw/ecs/swen225/gp22/recorder/Levels/" + maze.getLevel() + ".xml");
-	
+
 			duration = 100000;
 			startTime = -1;
-			
+
 			gui();
 			setVisible(true);
 		}
-		
-		
+
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-	
+
 		/**
 		 * Creates window for the main GUI
 		 */
 		public void gui() {
-			
+
 			JMenuBar tools = new JMenuBar();
-			
+
 			container = new JPanel();
 			JPanel camera = new JPanel();
 			JPanel info = new JPanel();
-	
+
 			JPanel infoLabels = new JPanel();
 			JPanel infoInv = new JPanel();
-			
+
 			infoLabels.setLayout(new GridLayout(5, 1));
 			infoInv.setLayout(new GridLayout(2,1));
-			
+
 			info.setLayout(new GridLayout(2, 1));
 			container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
-			
+
 			setResizable(false);
 			setSize(new Dimension(650, 450));
 			setMinimumSize(new Dimension(650, 450));		
-			
+
 			stop  = ()->{
 				this.dispose();
 				new Setup();
@@ -152,31 +152,41 @@
 				this.dispose();
 				new Game(level);
 			};
-			
+
 			nextLvl = ()->{
 				this.dispose();
 				String filename = "src/nz/ac/vuw/ecs/swen225/gp22/recorder/Levels/" + (maze.getLevel()+1) + ".xml";
 				new Game(new File(filename));
 			};
-			
+
 			JMenuItem stopbutton = new JMenuItem("stop");
 			stopbutton.addActionListener(e->stop.run());
-	
+
 			JMenuItem saveButton = new JMenuItem("Save");
-	        saveButton.addActionListener(e->save.run());
-	
+			saveButton.addActionListener(e->save.run());
+
+			JMenuItem nextButton = new JMenuItem("Next");
+			nextButton.addActionListener(e->Recorder.nextMove(maze));
+
+			JMenuItem autoButton = new JMenuItem("Auto-Play");
+			autoButton.addActionListener(e->Recorder.AutoPlay());
+
 			JLabel cl = new JLabel("TIME");
 			JLabel cd = new JLabel();
-	
+
 			JLabel pl = new JLabel("Patties Left");
 			JLabel pc = new JLabel();		
-			
+
 			JLabel infoTxt = new JLabel();
-			
+
 			JLabel inv = new JLabel("Inventory");
-	
+
+			JSlider speedControl = new JSlider(1, 5);
+			speedControl.setValue(1);
+			speedControl.addChangeListener(e->Recorder.playbackSpeed=speedControl.getValue());
+
 			timer = new Timer(10, new ActionListener() {
-	
+
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
@@ -188,8 +198,7 @@
 					if (clockTime >= duration) {
 						clockTime = duration;
 						timer.stop();
-						sound.playStung();
-						
+
 						String[] confirm = {"Quit", "Restart"};
 						JPanel panel = new JPanel();
 						JLabel label = new JLabel("You Lose!");
@@ -200,12 +209,12 @@
 						} else {
 							restart.run();
 						}
-						
+
 					}
-	
+
 					if(maze.hasWon()) {
-						sound.playWin();
 						timer.stop();
+						System.out.println("WHY NOT");
 						nextLvl.run();
 					}
 					if(maze.getChap().getStandingOn() instanceof InfoTile) {
@@ -213,25 +222,37 @@
 					} else {
 						infoTxt.setText("");
 					}
-					
+
 					cd.setText(" " + (int)((duration-clockTime)/1000));
 					pc.setText(" " + maze.checkTreasures());
-	
-					
+
+					if(Recorder.auto) {
+						Recorder.Next(maze);
+						try {
+							TimeUnit.MILLISECONDS.sleep(1000/Recorder.playbackSpeed);
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+					}
+
+
 					mv.repaint();
 					iv.repaint();
 				}
-				
+
 			});
 			if(!timer.isRunning()) {
-				sound.playAmbient();
 				timer.start();
 			}
-			
+
 			tools.add(stopbutton);
 			tools.add(saveButton);
+			tools.add(nextButton);
+			tools.add(autoButton);
+			tools.add(speedControl);
+
 			add(tools);
-			
+
 			infoLabels.add(cl);
 			infoLabels.add(cd);
 			infoLabels.add(pl);
@@ -239,26 +260,26 @@
 			infoLabels.add(inv);
 			infoInv.add(iv);
 			infoInv.add(infoTxt);
-	
-			
-	
+
+
+
 			getContentPane().add(tools, BorderLayout.PAGE_START);
 			camera.add(mv);	
-	
+
 			info.add(infoLabels);
 			info.add(infoInv);
-	
+
 			container.add(camera);
 			container.add(info);
 			add(container);
 			this.setPreferredSize(new Dimension(300,300));
-			
+
 			pack();
-	      	mv.requestFocus();
-			
+			mv.requestFocus();
+
 		}
-		
-		
-		
-	}
-	
+
+
+
+}
+
