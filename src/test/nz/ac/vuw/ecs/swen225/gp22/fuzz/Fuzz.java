44919c7ca1c2f827ed11a5bbcc55342990492196
Vedaanth Kannan <kannanveda@ecs.vuw.ac.nz>
2022-10-16 22:58:12 +1300

    Update src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
@@ -1,4 +1,4 @@
-package nz.ac.vuw.ecs.swen225.gp22.fuzz;
+package test.nz.ac.vuw.ecs.swen225.gp22.fuzz;
 
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
